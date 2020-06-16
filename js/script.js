ec5a0a0373cdd72849c076be941496fc54216e9b
Marco <Prosperi>
2020-06-16 18:03:47 +0200

    sto lavorando a js, ancora pieno di errori
    se non finisco stasera farò domani

--- js/script.js
+++ js/script.js
@@ -1,5 +1,10 @@
 $(document).ready(
-  function (){
-
-  }
+  $('.my_message_input').keyup(function() {
+    if (event.which === 13) {
+      var messaggio = $(this).val();
+      $('.1 .my_messages_right .my_mess_send .my_mess').text(messaggio);
+      $('.my_mess_send').clone().appendTo('.my_messages_right');
+      $('.my_message_input').val('');
+    }
+  })
 );
