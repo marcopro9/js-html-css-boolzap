adf60eb7f0f49dd01a60a92888fc3f67aa02ae69
Marco <Prosperi>
2020-06-17 11:32:22 +0200

    migliorati commenti, codice e sistemato invio messaggi

--- js/script.js
+++ js/script.js
@@ -1,9 +1,16 @@
+//attendo il caricamento del documento
 $(document).ready(
+  // prendo l'elemento my_message_input e al keyup faccio partire una funzione
   $('.my_message_input').keyup(function() {
+    // se viene premuto il tasto invio (13)...
     if (event.which === 13) {
+      // ...il valore di "my_message_input", inserito nella variabile,...
       var messaggio = $(this).val();
-      $('.1 .my_messages_right .my_mess_send .my_mess').text(messaggio);
-      $('.my_mess_send').clone().appendTo('.my_messages_right');
+      // ...viene inserito in un template nascosto...
+      $('.my_template .my_sent_message .my_text').text(messaggio);
+      // ...che viene clonato e attacato alla ul "my_chat"...
+      $('.my_template .my_sent_message').clone().appendTo('.my_chat');
+      // ...dopodichè il valore di "my_message_input" torna ad essere vuoto
       $('.my_message_input').val('');
     }
   })
